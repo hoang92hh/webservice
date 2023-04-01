8e81a84c828a36fa9639813f219e789eefdd129e
hoang92hh <hoang92honghoa@gmail.com>
2023-04-01 15:11:42 +0700

    use get set instea of lommbok data

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -2,8 +2,23 @@ package entity;
 
 import lombok.Data;
 
-@Data
+//@Data
 public class User {
+    public Integer getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     private Integer id;
     private String username;
